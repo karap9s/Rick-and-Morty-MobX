0f2c74cf18426f77cf613667874b35108dc00bfe
Daniil Sharenkov <Milkiy174@gmail.com>
2023-02-01 20:00:01 +0300

    init: folders structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
+import './App.css';
 
 const App: React.FC = () => {
   return (
-    <></>
+    <div className="wrapper">
+    <Header />
+    <Main />
+    <Footer />
+    </div>
   );
 };
 
