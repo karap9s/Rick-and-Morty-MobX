185def64cffd32806490dd98f9d13ea6bfc7a987
Daniil Sharenkov <Milkiy174@gmail.com>
2023-02-04 06:17:54 +0300

    feat: implement about page

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './About.css';
+import Daniil from '../../assets/img/Daniil.jpg';
 
 const About: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="about_wrapper">
+      <div className="about_card">
+        <img className="image" src={Daniil} alt="Rick" />
+        <h1 className="name">Daniil Sharenkov</h1>
+        <h3 className="species">Species: Human</h3>
+        <h3 className="gender">Gender: Male</h3>
+        <h3 className="status">Status: Alive</h3>
+        <button className="about_more">Show More</button>
+      </div>
+    </div>
+  );
 };
 
 export default About;
